dc25340df5dfb528207a5b0206549edadf953d78
Shahtr1 <tramboos123@gmail.com>
2020-11-06 22:24:49 +0530

    first commit

--- src/app/github-followers/github-followers.component.ts
+++ src/app/github-followers/github-followers.component.ts
@@ -1,5 +1,10 @@
 import { GithubFollowersService } from './../services/github-followers.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import 'rxjs/add/observable/combineLatest';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'github-followers',
@@ -9,10 +14,38 @@ import { Component, OnInit } from '@angular/core';
 export class GithubFollowersComponent implements OnInit {
   followers: any[];
 
-  constructor(private service: GithubFollowersService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private service: GithubFollowersService) { }
 
   ngOnInit() {
-    this.service.getAll()
-      .subscribe(followers => this.followers = followers);
+    Observable.combineLatest([
+      this.route.paramMap,
+      this.route.queryParamMap
+    ])
+    
+    // .subscribe(combined => {
+    //   let id = combined[0].get('id');
+    //   let page = combined[1].get('page');
+
+    //   // this.service.getAll({ id: id, page: page })
+    //   this.service.getAll()
+    //     .subscribe(followers => this.followers = followers);
+    // });
+
+    .switchMap(combined => {
+      let id = combined[0].get('id');
+      let page = combined[1].get('page');
+
+      return this.service.getAll();
+    })
+    .subscribe(followers => this.followers = followers);
+
+
+
+    // let id = this.route.snapshot.paramMap.get('id');
+    // let page = this.route.snapshot.queryParamMap.get('page');
+
+    
   }
 }
